7d5b73c5b61fcc7390e0de49e59c96f24254171c
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-08 22:32:46 -0300

    feat: add base project

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariants = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  height: 40px;
+  width: 100px;
+
+background-color: ${props => props.theme["green-500"]};
+
+/* 
+  ${(props) => {
+    return `background-color: ${buttonVariants[props.variant]}`;
+  }} */
+`;
