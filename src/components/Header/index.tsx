4a4f4f6392a605aa9da0e16417195a141f78efbb
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-11 17:01:28 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { ScrollTextIcon, TimerIcon } from "lucide-react";
+import { HeaderContainer } from "./style";
+import { NavLink } from "react-router-dom";
+
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+      <nav>
+        <NavLink to="/" title="Home">
+          <TimerIcon size={32} />
+        </NavLink>
+        <NavLink to="history" title="Histórico">
+          <ScrollTextIcon size={32} />{" "}
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+}
