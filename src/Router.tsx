b3ee2e0e22aebf9b809f52b813bbd25ef022a7ad
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-11 17:03:09 -0300

    feat: add header to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home";
 import { History } from "./pages/History";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 
+
 export function Router() {
   return (
     <Routes>
